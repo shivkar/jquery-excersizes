930dfa36efb523f2038ff5bd2b11adc1f4ea8846
shivkar <87944320+shivkar@users.noreply.github.com>
2022-08-09 18:51:08 -0400

    html browser elements

--- home.js
+++ home.js
@@ -1,16 +1,109 @@
 $(document).ready(function () {
 	
-	$('H1').addClass('text-center');
-	$('H2').addClass('text-center');
-    $('H1').removeClass('myBannerHeading');
-     $('H1').addClass('page-header');
-	 $("#yellowHeading").text("Yellow Team");
-	 $("#orangeTeamList").css('background-color', 'orange');
-	 $("#yellowTeamList").css('background-color', 'yellow');
-	 $("#redTeamList").css('background-color', 'red');
-	 $("#blueTeamList").css('background-color', 'blue');
-	 $("H1").hide();
-	 $(".footer").remove();
-	  $("#yellowTeamList").append('<li>Joseph Banks</li>');
-	   $('#yellowTeamList').append('<li>Simon Jones</li>');
+	 $("#mainInfoDiv").show();
+	$("#akronInfoDiv").hide();
+	$("#minneapolisInfoDiv").hide();
+	$("#louisvilleInfoDiv").hide();
+	
+		
+    
 });
+
+$("#akronButton").on('click', function() {
+		  $("#akronInfoDiv").show();
+		   $("#mainInfoDiv").hide();
+		  $("#akronWeatherButton").hide();
+		   $("#akronWeather").hide();
+		   $("#akronWeatherButton").show();
+		   $("#louisvilleInfoDiv").hide();
+	  $("#minneapolisInfoDiv").hide();
+        	    	 		  
+});
+
+
+		  
+ $("#minneapolisButton").on('click', function() {
+		  $("#minneapolisInfoDiv").show();
+		  $("#minneapolisWeatherButton").hide();
+		   $("#minneapolisWeather").hide();
+		    $("#mainInfoDiv").hide();
+			
+			$("#akronInfoDiv").hide();
+		  
+		  $("#akronWeatherButton").hide();
+		   $("#akronWeather").hide();
+		    $("#minneapolisWeatherButton").show();
+			$("#louisvilleInfoDiv").hide();
+	 $("#louisvilleWeatherButton").hide();
+	 $("#louisvilleWeather").hide();
+});
+
+
+$("#louisvilleButton").on('click', function() {
+	$("#louisvilleInfoDiv").show();
+	 $("#louisvilleWeatherButton").hide();
+	 $("#louisvilleWeather").hide();
+		  $("#minneapolisInfoDiv").hide();
+		  $("#minneapolisWeatherButton").hide();
+		   $("#minneapolisWeather").hide();
+		    $("#louisvilleWeatherButton").show();
+		    
+			$("#akronInfoDiv").hide();
+		  $("#akronWeatherButton").hide();
+		   $("#akronWeather").hide();
+         
+		   
+});	
+
+$("#akronWeatherButton").hover(
+        //in callback
+        function() {
+            $("#akronWeather").show();
+        },
+        //out callback
+        function() {
+            $("#akronWeather").hide();
+ });
+ 
+ $("#minneapolisWeatherButton").hover(
+        //in callback
+        function() {
+            $("#minneapolisWeather").show('slow');
+        },
+        //out callback
+        function() {
+            $("#minneapolisWeather").hide('slow');
+});
+
+$("#louisvilleWeatherButton").hover(
+        //in callback
+        function() {
+            $("#louisvilleWeather").show('slow');
+        },
+        //out callback
+        function() {
+            $("#louisvilleWeather").hide('slow');
+});
+$("#mainButton").on('click', function() {
+	
+	 $("#mainInfoDiv").show();
+	 $("#louisvilleInfoDiv").hide();
+	 
+});
+
+$(".table").hover(
+        //in callback
+        function() {
+            $("td").css('background-color', 'WhiteSmoke');
+        },
+        //out callback
+        function() {
+            $("td").css('background-color', 'white');
+
+});
+
+
+
+ 
+ 
+
